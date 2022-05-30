Added a parser argument --npatches and corrected the models' loss and dice calculation

two-seg.py
@@ -14,23 +14,24 @@ import save, losses, patches_func
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset')
+parser.add_argument('--npatches', default=16, type=int)
 args = parser.parse_args()
 
 ################## MODEL ##################
 
 model_1 = model = deeplabv3_resnet50(pretrained=False, progress=True, num_classes=1)
-model_1.load_state_dict(torch.load(f'results/ResNet50-{args.dataset}-patches-0.pth', map_location=torch.device('cpu')))
+model_1.load_state_dict(torch.load(f'results/ResNet50-{args.dataset.upper()}-patches-0.pth', map_location=torch.device('cpu')))
 model_1 = model_1.cuda()
 
 model_2 = model = deeplabv3_resnet50(pretrained=False, progress=True, num_classes=1)
-model_2.load_state_dict(torch.load(f'results/ResNet50-{args.dataset}-patches-1.pth', map_location=torch.device('cpu')))
+model_2.load_state_dict(torch.load(f'results/ResNet50-{args.dataset.upper()}-patches-1-{args.npatches}.pth', map_location=torch.device('cpu')))
 model_2 = model_2.cuda()
 
 ################## LOAD DATASET ##################
 
 i = importlib.import_module('dataloaders.' + args.dataset.lower())
 ds = getattr(i, args.dataset.upper())('test')
-patch_size = ds.hi_size // 16
+patch_size = ds.hi_size // args.npatches
 
 lo_transforms = A.Compose([
     A.Resize(ds.hi_size//8, ds.hi_size//8),
@@ -65,7 +66,7 @@ loss_func = nn.BCEWithLogitsLoss()
 
 model_1.eval()
 
-path = f'results/Two Segmentation Results/final-seg'
+path = f'results/Two Segmentation Results - {args.npatches}/final-seg'
 if not os.path.exists(path):
     os.makedirs(path)
 
@@ -80,7 +81,7 @@ dice_model2 = 0
 loss_modelF = 0
 dice_modelF = 0
 
-# Model 1
+##################### SEGMENTATION - STAGE 1 ####################
 for X_lo, Y_lo, X_hi, Y_hi in ts:
     X_lo = X_lo.cuda()
     Y_lo = Y_lo.cuda()
@@ -89,25 +90,25 @@ for X_lo, Y_lo, X_hi, Y_hi in ts:
 
     with torch.no_grad():
         Y_pred_lo = model_1(X_lo)['out']
-        Y_pred_hi_model1 = model_1(X_hi)['out']
+    
+    # Converting Y_pred_lo to high resolution (Y_pred_hi)
+    Y_pred_hi = torch.tensor(resize(Y_pred_lo.cpu()[0, 0], Y_hi.shape[2:])[None, None]).cuda()
+    Y_pred_hi = Y_pred_hi.cuda().detach().clone()
+ 
     if ds.nclasses > 2:
         dice = 0
-        loss = nn.functional.cross_entropy(Y_pred, Y)
+        loss = nn.functional.cross_entropy(Y_pred_hi, Y)
     else:
-        dice = losses.dice_score(torch.sigmoid(Y_pred_hi_model1), Y_hi)
-        loss = loss_func(Y_pred_hi_model1, Y_hi) + (1-dice)
+        dice = losses.dice_score(torch.sigmoid(Y_pred_hi), Y_hi)
+        loss = loss_func(Y_pred_hi, Y_hi) + (1-dice)
     loss_model1 += float(loss) / len(ts)
     dice_model1 += float(dice) / len(ts)
 
-    path = f'results/Two Segmentation Results/img{num}'
+    path = f'results/Two Segmentation Results - {args.npatches}/img{num}'
     if not os.path.exists(path):
         os.makedirs(path)
 
-    save.model1_result(X_lo, Y_lo, Y_pred_lo, num)
-
-    # converter a segmentacao Y_pred_lo para Y_pred_hi para estar na mesma resolucao da X_hi
-    Y_pred_hi = torch.tensor(resize(Y_pred_lo.cpu()[0, 0], Y_hi.shape[2:])[None, None])
-    Y_pred_hi1 = torch.tensor(resize(Y_pred_lo.cpu()[0, 0], Y_hi.shape[2:])[None, None])
+    save.model1_result(X_hi, Y_hi, Y_pred_hi, num, path)
 
     # Patch Division
     X_patch = torch.squeeze(X_hi).unfold(dimension=1, size=patch_size, step=patch_size).unfold(dimension=2, size=patch_size, step=patch_size)
@@ -116,14 +117,16 @@ for X_lo, Y_lo, X_hi, Y_hi in ts:
     _Y_pred_hi = Y_pred_hi if ds.nclasses == 2 else Y_pred_hi.max(1, keepdim=True)
     Y_pred_patch = torch.squeeze(_Y_pred_hi).unfold(dimension=0, size=patch_size, step=patch_size).unfold(dimension=1, size=patch_size, step=patch_size).cpu()
 
-    indices_val = patches_func.min10_avg(Y_pred_patch)
-    save.top10_result(Y_pred_patch, indices_val[0:10], args.dataset.upper(), 'pred', num)
+    #
+    indices_val = patches_func.avg_sorted(Y_pred_patch)
+    print(indices_val)
+    save.top_result(Y_pred_patch, indices_val, args.dataset.upper(), 'pred', num, path)
 
     n = 0
 
-    # Model 2
+    ##################### SEGMENTATION - STAGE 2 ####################
     model_2.eval()
-    for [i,j] in indices_val[0:10]:
+    for [i,j] in indices_val:
 
         X2 = X_patch[i,j].permute(2,0,1)[None,:,:,:].cuda()
         Y2 = Y_patch[i,j][None,None,:,:].cuda()
@@ -137,10 +140,10 @@ for X_lo, Y_lo, X_hi, Y_hi in ts:
         else:
             dice = losses.dice_score(torch.sigmoid(Y_pred2), Y2)
             loss = loss_func(Y_pred2, Y2) + (1-dice)
-        loss_model2 += float(loss) / (10*len(ts))
-        dice_model2 += float(dice) / (10*len(ts))
+        loss_model2 += float(loss) / (len(indices_val)*len(ts))
+        dice_model2 += float(dice) / (len(indices_val)*len(ts))
         
-        save.model2_result(Y2, Y_pred_patch[i,j], Y_pred2, num, n)
+        save.model2_result(Y2, Y_pred_patch[i,j], Y_pred2, num, n, path)
         
         Y_pred2 = torch.squeeze(Y_pred2)
         Y_pred_patch[i,j] = Y_pred2
@@ -160,12 +163,11 @@ for X_lo, Y_lo, X_hi, Y_hi in ts:
 
     # Save images
     final_mask = torch.squeeze(final_mask).cpu()
-    save.fig(final_mask, 'final_mask', num)
-    
-    # FIXME: Não percebo porque é que, se tentar fazer save.TWOseg_result(Y_hi, Y_pred_hi, final_mask) o Y_pred_hi é igual à final mask (??) Para contornar isso tive de criar uma outra variável, a Y_pred_hi1 (se colocasse Y_pred_hi1 = Y_pred_hi no mesmo local onde declarei Y_pred_hi1 teria o mesmo problema de cima ?? )
-    save.TWOseg_result(Y_hi, Y_pred_hi1, final_mask, patch_size, indices_val, num)
+    save.fig(final_mask, 'final_mask', num, path)
+    save.TWOseg_result(Y_hi, Y_pred_hi, final_mask, patch_size, indices_val, num, path)
     num += 1
 
-print(f'Model 1 Loss: {loss_model1} - Dice: {dice_model1}')
-print(f'Model 2 Loss: {loss_model2} - Dice: {dice_model2}')
-print(f'Model 1+2 Loss: {loss_modelF} - Dice: {dice_modelF}')
+f = open(f'results/Two Segmentation Results - {args.npatches}/dice.txt', 'w')
+print(f'Model 1 Loss: {loss_model1} - Dice: {dice_model1}', file=f)
+print(f'Model 2 Loss: {loss_model2} - Dice: {dice_model2}', file=f)
+print(f'Model 1+2 Loss: {loss_modelF} - Dice: {dice_modelF}', file=f)