01549a86f975f237c09e1aa4d45419b792bcda07
Diana Silva <up201805131@g.uporto.pt>
2022-06-12 17:38:50 +0100

    added a few plots in the end

--- train.py
+++ train.py
@@ -3,9 +3,12 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.models.segmentation import deeplabv3_resnet50
 from torch import optim
+from torchvision.ops.focal_loss import sigmoid_focal_loss
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from time import time
+from tqdm import tqdm
+from torchinfo import summary
 import datetime
 import importlib
 import losses
@@ -14,20 +17,11 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset')
 parser.add_argument('use_patches', choices=[0, 1], type=int)
-parser.add_argument('--npatches', default=16, type=int)
-parser.add_argument('--model', default='deeplab')
+parser.add_argument('model', choices=['deeplab', 'brain', 'ours'])
+parser.add_argument('--npatches', default=1, type=int)
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-################## MODEL ##################
-
-if args.model == 'deeplab':
-    model = deeplabv3_resnet50(pretrained=False, progress=True, num_classes=1)
-else:
-    model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
-        in_channels=3, out_channels=1, init_features=32, pretrained=True)
-model = model.to(device)
 
 ################## LOAD DATASET ##################
 
@@ -36,9 +30,10 @@ ds = getattr(i, args.dataset.upper())
 
 l = [
     A.HorizontalFlip(),
-    A.Rotate(180),
     A.RandomBrightnessContrast(0.1),
 ]
+if ds.can_rotate:
+    l += [A.Rotate(180)]
 if args.use_patches: # low-resolution
     l += [
         A.Resize(ds.hi_size, ds.hi_size),
@@ -55,6 +50,21 @@ train_transforms = A.Compose(l)
 tr = ds('train', transform=train_transforms)
 pos_weight = ds.pos_weight
 
+
+################## MODEL ##################
+
+if args.model == 'deeplab':
+    model = deeplabv3_resnet50(pretrained=False, progress=True, num_classes=1)
+elif args.model == 'brain':
+    model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
+        in_channels=ds.colors, out_channels=1, init_features=32, pretrained=True)
+elif args.model == 'ours':
+    from unet import UNet
+    model = UNet(ds.colors)
+model = model.to(device)
+
+summary(model, (10, ds.colors, ds.hi_size//args.npatches, ds.hi_size//args.npatches))
+
 ################## TRAINING ##################
 
 tr = DataLoader(tr, batch_size=64, shuffle=True, num_workers=6)
@@ -62,30 +72,44 @@ learning_rate = 1e-4
 opt = optim.Adam(model.parameters(), learning_rate)
 
 loss_func = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight))
+#loss_func = lambda ypred, y: sigmoid_focal_loss(ypred, y, reduction='mean')
 
 # Training the model
 
 model.train()
 print (f'\nTraining {args.dataset} USEPATCHES={args.use_patches} dataset...\n')
 
-EPOCHS = 300
-if args.use_patches:
-    EPOCHS *= 16
+EPOCHS = 200
+#if args.use_patches:
+#    EPOCHS *= 16
 
 total_time = 0
 loss_values =[]
 epoch_values = []
+acc_total = []
+acc_0 = []
+acc_1 = []
+dice_values = []
+
 for epoch in range(EPOCHS):
     print(f'* Epoch {epoch+1} / {EPOCHS}')
     tic = time()
     avg_loss = 0
     avg_dice = 0
-    for X, Y in tr:
+    avg_acc = 0
+    avg_acc0 = 0
+    avg_acc1 = 0
+    for X, Y in tqdm(tr):
         X = X.to(device)
         Y = Y[:, None, :,:].to(device)
         #print('X:', X.shape)
         #print('Y:', Y.shape)
-        Y_pred = model(X)['out']
+
+        if args.model == 'deeplab' and ds.colors == 1:
+            X = torch.cat((X, X, X), 1)
+        Y_pred = model(X)
+        if args.model == 'deeplab':
+            Y_pred = Y_pred['out']
         #print('Y_pred:', Y_pred.shape)
         if ds.nclasses > 2:
             dice = 0
@@ -98,11 +122,19 @@ for epoch in range(EPOCHS):
         opt.step()
         avg_loss += float(loss) / len(tr)
         avg_dice += float(dice) / len(tr)
+        acc = (Y >= 0.5) == (Y_pred >= 0.5)
+        avg_acc += float(torch.mean(acc.float())) / len(tr)
+        avg_acc0 += float(torch.sum((acc & (Y == 0)).float()) / torch.sum(Y == 0)) / len(tr)
+        avg_acc1 += float(torch.sum((acc & (Y == 1)).float()) / torch.sum(Y == 1)) / len(tr)
     toc = time()
     total_time += toc-tic
     loss_values.append(avg_loss)
     epoch_values.append(epoch+1)
-    print(f'- Time: {toc-tic:.1f}s - Loss: {avg_loss} - Dice score: {avg_dice} ')
+    acc_total.append(avg_acc)
+    acc_0.append(avg_acc0)
+    acc_1.append(avg_acc1)
+    dice_values.append(avg_dice)
+    print(f'- Time: {toc-tic:.1f}s - Loss: {avg_loss} - Dice score: {avg_dice} - Accuracy: {avg_acc} ({avg_acc0} vs {avg_acc1})')
 
 total_time = str(datetime.timedelta(seconds=round(total_time)))
 
@@ -117,9 +149,29 @@ else:
 ################## PLOT ##################
 
 import matplotlib.pyplot as plt
+
+# Plotting Loss vs Epoch
 fig = plt.figure(figsize=(10,5))
 plt.plot(epoch_values, loss_values)
 plt.title(f'{args.dataset} {args.use_patches} - Training Time = {total_time} \n Learning rate = {learning_rate}')
 plt.xlabel('EPOCH')
 plt.ylabel('Loss')
 fig.savefig(f'results/ResNet50_{args.dataset}_patches_{args.use_patches}_{args.npatches}train_loss.png',bbox_inches='tight', dpi=150)
+
+# Plotting Accuracy vs Epoch
+fig = plt.figure(figsize=(10,5))
+plt.plot(epoch_values, acc_total, label='Total Accuracy')
+plt.plot(epoch_values, acc_0, label='0-pixels Accuracy')
+plt.plot(epoch_values, acc_1, label='1-pixels Accuracy')
+plt.xlabel('EPOCH')
+plt.ylabel('Accuracy')
+plt.legend()
+fig.savefig(f'results/ResNet50_{args.dataset}_patches_{args.use_patches}_{args.npatches}train_acc.png',bbox_inches='tight', dpi=150)
+
+# Plotting Dice vs Epoch
+fig = plt.figure(figsize=(10,5))
+plt.plot(epoch_values, dice_values)
+plt.xlabel('EPOCH')
+plt.ylabel('Dice')
+fig.savefig(f'results/ResNet50_{args.dataset}_patches_{args.use_patches}_{args.npatches}train_dice.png',bbox_inches='tight', dpi=150)
+
